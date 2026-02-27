bb42554765963cf3a23f5573f771979a46ec9199
stphic24 <philipp.straub@reutlingen-university.de>
2026-02-27 17:20:19 +0100

    Added Taxonomy of QAs and Tactics to Model

--- semantic model development/foundational_semantic_model_generator/src/foundational_semantic_model_generator/main.py
+++ semantic model development/foundational_semantic_model_generator/src/foundational_semantic_model_generator/main.py
@@ -49,16 +49,39 @@ def main():
         df_tactics = pd.read_excel(tactic_file_path)
         print(f"'INFO | Successfully loaded {tactic_excel_file}'.")
 
+        # Extract unique types and create subClassOf triples
+        unique_types = df_tactics['Type'].dropna().unique()
+        for tactic_type in unique_types:
+            type_subject = str(tactic_type).replace(" ", "_")
+            # type_triple = Triple(type_subject, "rdf:type", "rdfs:Class")
+            subclass_triple = Triple(type_subject, "rdfs:subClassOf", "Tactic")
+            # turtle_content.append(str(type_triple))
+            turtle_content.append(str(subclass_triple))
+
+        # Extract unique categories
+        unique_categories = df_tactics['Category'].dropna().unique()
+        for category_type in unique_categories:
+            category_subject = str(category_type).replace(" ", "_")
+            category_triple = Triple(category_subject, "rdf:type", "Category")
+            turtle_content.append(str(category_triple))
+
+
+        # Extract each tactic as triple
         for index, row in df_tactics.iterrows():
             tactic = row['Tactics']
             tactic_type = row['Type']
+            tactic_category = row['Category']
             
             if pd.notna(tactic):
                 subject = str(tactic).replace(" ", "_").replace("/", "_or_").replace(",", "_or_")
-                obj = str(tactic_type).replace(" ", "_")
+                obj_type = str(tactic_type).replace(" ", "_")
+                obj_category = str(tactic_category).replace(" ", "_")
                 
-                triple = Triple(subject, "rdf:type", obj)
-                turtle_content.append(str(triple))
+                triple_type = Triple(subject, "rdf:type", obj_type)
+                triple_cat = Triple(subject, "hasCategory", obj_category)
+                turtle_content.append(str(triple_type))
+                turtle_content.append(str(triple_cat))
+        
 
     except FileNotFoundError:
         print(f"ERROR | No file {tactic_excel_file} in the folder {input_folder} available.")
@@ -70,14 +93,27 @@ def main():
         df_qa = pd.read_excel(qa_file_path)
         print(f"'INFO | Successfully loaded {qa_excel_file}'.")
         
+
+        # Extract each qa as triple
         for index, row in df_qa.iterrows():
             quality_attribute = row['Quality Attribute']
+            qa_type = row['Type according to ISO25059']
+            qa_part_of_characteristic = row['Part of Charateristic']
 
             if pd.notna(quality_attribute):
                 subject = str(quality_attribute).replace(" ", "_").replace("/", "_or_").replace(",", "_or_")
+                obj_type = str(qa_type).replace(" ", "_").replace("/", "_or_").replace(",", "_or_")
 
-                triple = Triple(subject, "rdf:type", "QualityAttribute")
+                triple = Triple(subject, "rdf:type", obj_type)
                 turtle_content.append(str(triple))
+                if obj_type == "SubCharacteristic":
+                    subject_char = str(qa_part_of_characteristic).replace(" ", "_").replace("/", "_or_").replace(",", "_or_")
+                    object = subject
+                    triple_char = Triple(subject_char, "hasSubQualityAttribute", object)
+                    turtle_content.append(str(triple_char))
+                    
+
+
     except FileNotFoundError:
         print(f"ERROR | No file {qa_excel_file} in the folder {input_folder} available.")
     except Exception as e:
@@ -98,9 +134,17 @@ def main():
             f.write("@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n")
             
             f.write(":Tactic rdf:type rdfs:Class .\n")
-            f.write(":DesignTactic rdfs:subClassOf :Tactic .\n")
-            f.write(":ImplementationTactic rdfs:subClassOf :Tactic .\n")
+            f.write(":Category rdf:type rdfs:Class .\n")
+            # f.write(":DesignTactic rdfs:subClassOf :Tactic .\n")
+            # f.write(":ImplementationTactic rdfs:subClassOf :Tactic .\n")
+            f.write(":hasCategory rdf:type rdf:Property ; rdfs:domain :Tactic ; rdfs:range :Category .\n\n")
+            
             f.write(":QualityAttribute rdf:type rdfs:Class .\n\n")
+            f.write(":Characteristic rdf:type rdfs:Class ; rdfs:subClassOf :QualityAttribute .\n\n")
+            f.write(":SubCharacteristic rdf:type rdfs:Class ; rdfs:subClassOf :Characteristic .\n\n")
+            f.write(":hasSubQualityAttribute rdf:type rdf:Property ; rdfs:domain :QualityAttribute ; rdfs:range :QualityAttribute .\n\n")
+
+
             for line in turtle_content:
                 f.write(line + "\n")
 
